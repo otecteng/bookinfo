6f485b3a299fca24b8f92b79889a8cd5aae0e2fe
Sicong He <sicong.he1@Sicongs-MBP.domain.name>
2021-04-29 23:53:05 +0800

    提交域名变更

--- dotnet/productpage/Startup.cs
+++ dotnet/productpage/Startup.cs
@@ -73,7 +73,7 @@ namespace productpage
             services.AddNacosAspNetCore(Configuration);
             services.AddNacosV2Naming(x =>
             {
-                x.ServerAddresses = new System.Collections.Generic.List<string> { "http://localhost:8848/" };
+                x.ServerAddresses = new System.Collections.Generic.List<string> { "http://40.72.115.89:8848/" };
                 x.EndPoint = "";
                 // swich to use http or rpc
                 x.NamingUseRpc = true;
